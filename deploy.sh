08a378b6f8d5934384cd6a70085bd44ac3e710be
destinytaoer <1848765519@qq.com>
2019-02-14 19:36:22 +0800

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,13 @@ git add -A
 git commit -m $1
 
 # 将 dist 文件提交到 gh-pages 分支
-git subtree push --prefix dist origin gh-pages 
+# git subtree push --prefix dist origin gh-pages 
+git subtree split --rejoin --prefix dist --branch gh-pages
 
 # 提交到 master
-git push origin master 
+git push origin gh-pages:gh-pages
+
+git push origin master:master
 
 cd -
 
