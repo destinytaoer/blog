968aecb2701b4dd132e6ee0e4b32f75623140dbe
destinytaoer <1848765519@qq.com>
2019-02-18 01:01:54 +0800

    add pagination

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,9 @@ git commit -m $1
 
 # 将 dist 文件提交到 gh-pages 分支
 # git subtree push --prefix dist origin gh-pages 
-git subtree split --rejoin --prefix dist --branch gh-pages
 
 # 提交到 master
-git push origin gh-pages:gh-pages
+# git push origin gh-pages:gh-pages
 
 git push origin master:master
 
