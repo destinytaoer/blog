e44ed8e604a9db082b83bf6bdf291ee46806108f
destinytaoer <1848765519@qq.com>
2019-02-18 17:37:35 +0800

    修改 pagination 为事件控制 index

---
+++ assets/js/54.f7b09576.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[54],{331:function(n,w,o){}}]);
--- assets/js/7.db54423b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{339:function(t,e,n){"use strict";n.r(e);var s={data:function(){return{}},beforeMount:function(){console.log(this.$page,this.$site)}},i=n(1),o=Object(i.a)(s,function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"categories"})},[],!1,null,"2c8ae716",null);e.default=o.exports}}]);
